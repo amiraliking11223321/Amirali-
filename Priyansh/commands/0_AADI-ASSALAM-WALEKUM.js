cb5b3e249b494c3fc610299a6226ce5a6ee81d5e
amiraliking11223321 <amiraliking11223344@gmail.com>
2025-04-03 23:24:12 +0500

    Update 0_AADI-ASSALAM-WALEKUM.js

--- Priyansh/commands/0_AADI-ASSALAM-WALEKUM.js
+++ Priyansh/commands/0_AADI-ASSALAM-WALEKUM.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "BYE",
     version: "1.1.1",
         hasPermssion: 0,
-        credits: "AADI BABU", 
+        credits: honey king", 
         description: "THIS BOT IS AADI SHARMA",
         commandCategory: "no prefix",
     cooldowns: 5, 
@@ -17,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 react.includes("Assalam alaikum") ||
 react.includes("assalamualaikum")) {
                 var msg = {
-                                body: `____________________________________\n\nWalekum Aaslamu  Wa Ramutllha Barkatu 😇\n____________________________________ \n𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝙎𝙝𝘼𝙖𝙉   𝙆𝙝𝘼𝙣`,
+                                body: `____________________________________\n\nWalekum Aaslamu  Wa Ramutllha Barkatu 😇\n____________________________________ \n𝐎𝐰𝐧𝐞𝐫 ➻  ────  𝐇𝐚𝐦𝐳𝐚 𝐀𝐥𝐢`,
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🤲", event.messageID, (err) => {}, true)
